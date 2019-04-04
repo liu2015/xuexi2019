15997fb37f0fed9814a15ba80c47d8ec8cf9af6a
ZHY <liufuling14@163.com>
2019-04-04 21:20:12 +0800

    20190404

---
+++ untitled/src/Phone.java
@@ -0,0 +1,14 @@
+public class Phone {
+    String brand;
+    int price;
+    String color;
+
+    public void call(String name){
+        System.out.println("gei "+name+"da dianhua"+price+"shi shenm yanse "+color);
+
+    }
+    public void senMessage(){
+
+        System.out.println("gei shui duanxin");
+    }
+}
---
+++ untitled/src/Student.java
@@ -0,0 +1,18 @@
+public class Student {
+    String sid;
+    String name;
+    int age;
+    public void study(){
+        System.out.println("学生爱学习");
+
+    }
+
+    public  void eat(){
+        System.out.println("学生要吃饭");
+
+    }
+    public void sleep (){
+
+        System.out.println("学生想睡觉");
+    }
+}
